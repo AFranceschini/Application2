1bb3fbe1da9dfd290fd144a971668dd58bac23cd
AFranceschini <antoinefranceschini@pc26.home>
2015-10-05 01:04:09 +0200

    Test4
    Test

--- www/config.js
+++ www/config.js
@@ -5,9 +5,9 @@ define( function ( require ) {
 	return {
 		app_slug : 'application',
 		wp_ws_url : 'http://vps78594.ovh.net/mobile/wp-appkit-api/application',
-		theme : 'nihao-ios',
-		version : '1.1.0',
-		app_title : 'My APP',
+		theme : 'bootstrap',
+		version : '1.0.0',
+		app_title : 'Application',
 		debug_mode : 'off',
 		auth_key : '^dFk!qL4mrJ!bTBK@Z!ESguP87JIoiHacXG(WpjtrsWpSvSzof*hy(@8MClMhueM',
 		options : {"refresh_interval":0},
