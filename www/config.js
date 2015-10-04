39a9a6e531edd3a29757fd1ff094f6f4d37a451c
AFranceschini <antoinefranceschini@pc26.home>
2015-10-05 00:51:31 +0200

    Test
    Test

---
+++ www/config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'application',
+		wp_ws_url : 'http://vps78594.ovh.net/mobile/wp-appkit-api/application',
+		theme : 'nihao-ios',
+		version : '1.1.0',
+		app_title : 'My APP',
+		debug_mode : 'off',
+		auth_key : '^dFk!qL4mrJ!bTBK@Z!ESguP87JIoiHacXG(WpjtrsWpSvSzof*hy(@8MClMhueM',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
